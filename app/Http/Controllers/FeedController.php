823695ae1f53d882a215c6a02928d18849868976
Kondin Dmitriy <dkondin@gmail.com>
2020-09-04 17:21:46 +0700

    отдаем xml

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 
 class FeedController extends Controller
 {
     public function index (Request $request) {
         // Create options for the API
+        $guid = $request->get('guid');
+
         $shop = User::where('guid', $request->get('guid'))->firstOrFail();
+        return response(Storage::get($guid.'.xml'), 200, [
+            'Content-Type' => 'application/xml'
+        ]);
 
         $result = $shop->api()->rest('GET', '/admin/products.json');
         Log::info(json_encode($result));
